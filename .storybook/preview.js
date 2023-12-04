3f86a36a091c0916c447d2e3f93a2602d3988498
知念英弥 <s19013@std.it-college.ac.jp>
2023-12-05 00:41:19 +0900

    vuetifyと連携設定

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,19 @@
 /** @type { import('@storybook/vue3').Preview } */
 import { setup } from '@storybook/vue3';
-import { registerPlugins } from '../src/plugins';
-import { withVuetifyTheme } from './withVuetifyTheme.decorator';
+import { withVuetifyTheme } from './withVeutifyTheme.decorator';
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 setup((app) => {
-  registerPlugins(app);
+  app.use(vuetify)
 });
 
 export const decorators = [withVuetifyTheme];
