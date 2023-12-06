961d5a03fea862f6084923b02ba6f3d45fceef77
知念英弥 <s19013@std.it-college.ac.jp>
2023-12-06 23:50:00 +0900

    globalsを有効にした

--- vitest.config.js
+++ vitest.config.js
@@ -2,13 +2,15 @@ import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
+// vite.configファイルに以下の設定を追加している
+
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      environment: 'jsdom',
-      include: ['test/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-      root: fileURLToPath(new URL('./', import.meta.url))
+        globals: true,
+        include: ['./test/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+        root: fileURLToPath(new URL('./', import.meta.url))
     }
   })
 )
